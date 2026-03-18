2058677b38f6621d48855f6fd7779a0d83d6be50
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-03-18 11:02:48 +0100

    Study on available libero tasks

---
+++ libero-study/task-discovery.py
@@ -0,0 +1,158 @@
+import collections
+import os
+from typing import Dict, Generator, List, Tuple, Type
+
+from attr import dataclass
+import imageio
+from libero.libero import benchmark
+from libero.libero.benchmark import Benchmark, Task
+from libero.libero.envs.env_wrapper import ControlEnv, OffScreenRenderEnv
+import numpy
+
+HEADLESS = True
+
+CAMERA_NAME = "agentview"
+CAMERA_SIZE = 512
+DUMMY_ACTION = [0.] * 7 
+
+def load_task(benchmark_suite: str, task_id: int) -> Tuple[Benchmark, Task]:
+    ''' Load a spedfic task from a specific benchmark of LIBERO
+
+        Parameters
+        -------------- 
+        benchmark_suite: str
+            The benchmark/task suite to load. Available benchmarks are:
+
+                `libero_spatial`
+                    for spatial understaning, same task different objects
+                `libero_object`
+                    different layout with same objects
+                `libero_goal`
+                    different goal, same layout and objs
+                `libero_90` 
+                    training set of `libero_100`
+                `libero_10`
+                    eval set of `libero_100`
+                `libero_100`
+                    different objects, layouts and scenes
+        task_id: int
+            The id of the task to load
+
+        Returns
+        -------
+        A Tuple[Benchmark, Task] or a generated list of Tuple[Benchmark, Task] for each possible task.
+
+        A Task object is simply a tuple of:
+        - `name`: task name (_ separated)
+        - `language`: tasbenchmark.k lang instruction
+        - `problem`: libero tasks are under `libero` problem
+        - `problem_folder`: the specific `libero_benckmark` folder name
+        - `bddl_file`: the full .bbdl file name (no path)
+        - `init_states_file`: the .init file name for the task
+    '''
+    
+    benchmark_dict: Dict[str, Type[Benchmark]] = benchmark.get_benchmark_dict()
+    ''' The `benchmark_dict` (mapped with the variable `benchmark.BENCHMARK_MAPPING`) 
+        containst the available Libero Benchmarks Suites, that are:
+        - `libero_spatial` : for spatial understaning, same task different objects
+        - `libero_object`: different layaot with same objects
+        - `libero_goal` : different goal, same layout and objs
+        - `libero_90` : training set of `libero_100`
+        - `libero_10` : eval set of `libero_100`
+        - `libero_100` : different objects, layouts and scenes 
+    '''
+
+    task_suite: Benchmark = benchmark_dict[benchmark_suite]()
+    '''`task_suite` will be an instance of one of the available Benchmarks classes
+        Each benchmark has 10 tasks, except `benchmark.LIBERO_90` which has 90 tasks
+    '''
+
+    # Print information about the selected benchmark
+    print(f"Loaded: {task_suite}, {task_suite.get_num_tasks()} tasks available")
+    print(f"Available tasks: {task_suite.get_task_names()}")
+
+    # Load a specific task
+    task: Task = task_suite.get_task(task_id)
+    return task_suite, task
+    
+def load_tasks(benchmark_suite: str) -> Generator[Tuple[Benchmark, Task], None, None]:
+    # Return the first task
+    task_suite, task = load_task(benchmark_suite, 0)
+    yield task_suite, task
+    # Generate and return all the remaining tasks
+    for task_id in range(1, task_suite.get_num_tasks()):
+        task = task_suite.get_task(task_id)
+        yield task_suite, task
+
+def load_environment(task: Task) -> ControlEnv:
+
+    # Load bddl file
+    BDDL_FILE_PATH = os.path.join(benchmark.get_libero_path("bddl_files"), task.problem_folder, task.bddl_file)
+    #print(f"Attempting to open {BDDL_FILE_PATH}")
+
+    env_args = {
+        "bddl_file_name": BDDL_FILE_PATH,
+        "camera_heights": CAMERA_SIZE,
+        "camera_widths": CAMERA_SIZE
+    }
+
+    if HEADLESS:
+        env = OffScreenRenderEnv(**env_args)
+    else:
+        env = ControlEnv(BDDL_FILE_PATH, 
+                         has_renderer=True, # On screen view
+                         has_offscreen_renderer=True, # If camera or headless, an offscreen is required
+                         render_camera=CAMERA_NAME,
+                         camera_heights=CAMERA_SIZE,
+                         camera_widths=CAMERA_SIZE
+                         )
+    return env
+
+@dataclass
+class TaskEntry:
+    task_name: str
+    task_instruction: str
+    
+
+@dataclass
+class BenchmarkEntry:
+    tasks: Dict[int, TaskEntry]
+
+    def to_json_dict(self):
+        return{task_id:task for task_id, task in self.tasks.items()}
+
+
+def main():
+    # Load every benchmark
+    for benchmark_suite in benchmark.BENCHMARK_MAPPING.keys():
+        
+        # Load all tasks
+        for bench, task in load_tasks(benchmark_suite):
+            print(f"Loading task: {task.name}")
+            print(f"Instruction: {task.language}")
+            # Load task environment
+            task_env = load_environment(task)
+            # Reset env
+            task_env.seed(0)
+            obs: collections.OrderedDict = task_env.reset()
+
+            # step over the environment
+            frame_buffer: List[numpy.ndarray] = []
+            for step in range(100):
+                if not HEADLESS: task_env.env.render()
+                if step % 100 == 0: print("*", end="", flush=True)
+                
+                # Execute dummy action (randomly open-close gripper)
+                DUMMY_ACTION[-1] = numpy.random.uniform(-1, 1)
+                obs, reward, done, info = task_env.step(DUMMY_ACTION)
+                
+                # Apparently, we need to flip images from obs :-) :-| :-< >:-<
+                agentview_image = obs["agentview_image"][::-1]
+                wrist_image = obs["robot0_eye_in_hand_image"][::-1]
+
+            print(f"Observations: {obs.keys()}")
+            task_env.close()
+
+    
+if __name__ == "__main__":
+    main()
