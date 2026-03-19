1e228ef941c144224c1a74384d2524298d40d27c
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-03-19 12:40:57 +0100

    Creates new task

--- libero-study/tasks/custom-task.py
+++ libero-study/tasks/custom-task.py
@@ -1,5 +1,6 @@
 
 import re
+from tkinter import OFF
 from typing import ClassVar, Dict, List, Tuple, Type
 
 from libero.libero.envs.predicates import VALIDATE_PREDICATE_FN_DICT
@@ -13,8 +14,8 @@ import numpy
 
 GENERATED_BDDL_PATH = "/home/peraro/source/play-libero/libero-study/tasks"
 
-print("Objects:", OBJECTS_DICT.keys())
-print("Predicates:", VALIDATE_PREDICATE_FN_DICT.keys())
+"""print("Objects:", OBJECTS_DICT.keys())
+print("Predicates:", VALIDATE_PREDICATE_FN_DICT.keys())"""
 
 """
     This file creates a scene with 9 plates, a wooden cabinet and a cookie box.
@@ -214,35 +215,155 @@ class DrawerPlateCookieScene(InitialSceneTemplates):
         return [
             "Pick up cookie box and place it on the top right plate"
         ]
+    
+    @classmethod
+    def goal_states(cls) -> List[List[Tuple[str, str, str]]]:
+        # Define a set of goal statements for every instruction
+        all_goals=[]
+        # Instruction 1
+        goal_1: List[Tuple[str, str, str]] = [("On", "cookies_1", "plate_3")]
+        
+        all_goals.append(goal_1)
+        return all_goals
+
+@register_mu(scene_type="kitchen")
+class PlateCookieScene(InitialSceneTemplates):
+    """
+        A scene with three cookiebox and a central plate
+
+        Scheme wrt agentview camera:
+            
+            c1 p c2
+               c3 
+    """
+
+    def __init__(self):
+        fixtures = {
+            "kitchen_table": 1,
+        }
+
+        objs = {
+            "plate": 1,
+            "cookies": 3
+        }
+
+        super().__init__(
+            workspace_name="kitchen_table",
+            fixture_num_info=fixtures,
+            object_num_info=objs
+        )
+        
+    def define_regions(self):
+
+        plate_region = self.get_region_dict(
+            region_centroid_xy=[0.0, 0],
+            region_name="plate_region",
+            target_name=self.workspace_name,
+            region_half_len=0.0001
+        )
+
+        OFFSET = 0.15
+        cookies_region_1 = self.get_region_dict(
+            region_centroid_xy=[0.0, -OFFSET],
+            region_name="cookies_region_1",
+            target_name=self.workspace_name,
+            region_half_len=0.0001
+        )
+
+        cookies_region_2 = self.get_region_dict(
+            region_centroid_xy=[0, OFFSET],
+            region_name="cookies_region_2",
+            target_name=self.workspace_name,
+            region_half_len=0.0001
+        )
+
+        cookies_region_3 = self.get_region_dict(
+            region_centroid_xy=[OFFSET, 0],
+            region_name="cookies_region_3",
+            target_name=self.workspace_name,
+            region_half_len=0.0001
+        )
+
+        self.regions.update(plate_region)
+        self.regions.update(cookies_region_1)
+        self.regions.update(cookies_region_2)
+        self.regions.update(cookies_region_3)
+
+        self.xy_region_kwargs_list = get_xy_region_kwargs_list_from_regions_info(self.regions)
+
+    @property
+    def init_states(self):
+        states: List[Tuple[str, ...]]
+        """A list of valid initialization statement.
+            A statement is made of:
+            - predicate one from VALIDATE_PREDICATE_FN_DICT
+            - arg(s) one or more arguments for the predicate
+
+            An arg can be an object or a target region in case 
+        """
+        states = [
+            ("On", "cookies_1", "kitchen_table_cookies_region_1"),
+            ("On", "cookies_2", "kitchen_table_cookies_region_2"),
+            ("On", "cookies_3", "kitchen_table_cookies_region_3"),
+            ("On", "plate_1", "kitchen_table_plate_region"),
+        ]
+        return states
+    
+    @classmethod
+    def get_scene_name(cls) -> str:
+        """ Returns the class name in LIBERO format. Useful to have when registering the class"""
+        return "_".join(re.sub(r"([A-Z])", r" \1", cls.__name__).split()).lower()
+    
+    @classmethod
+    def scene_instructions(cls) -> List[str]:
+        """ Returns available task instructions for this scene"""
+        return [
+            "Pick up all cookie boxes and place them on the plate"
+        ]
+    
+    @classmethod
+    def goal_states(cls) -> List[List[Tuple[str, str, str]]]:
+        # Define a set of goal statements for every instruction
+        all_goals=[]
+        # Instruction 1
+        goal_1: List[Tuple[str, str, str]] = [
+            ("On", "cookies_1", "plate_1"),
+            ("On", "cookies_2", "plate_1"),
+            ("On", "cookies_3", "plate_1"),
+        ]
+        
+        all_goals.append(goal_1)
+        return all_goals      
         
 
 def main():
-
-    bddl_file_names = []
-    failures  = []
  
     # Register DrawerPlateCookieScene
     register_task_info(
         DrawerPlateCookieScene.scene_instructions()[0], # type: ignore
         DrawerPlateCookieScene.get_scene_name(), # type: ignore
         objects_of_interest=["cookies_1"],
-        goal_states=[
-            ("On", "cookies_1", "plate_3")
-        ]
+        goal_states=DrawerPlateCookieScene.goal_states()[0] # type:ignore
     )
+
+    register_task_info(
+        PlateCookieScene.scene_instructions()[0], # type: ignore
+        PlateCookieScene.get_scene_name(), # type: ignore
+        objects_of_interest=["cookies_1"],
+        goal_states=PlateCookieScene.goal_states()[0] # type:ignore
+    )
+
 
     bddl_file_names, failures = generate_bddl_from_task_info(folder=GENERATED_BDDL_PATH)
     print(f"Generated {bddl_file_names}")
     print(f"Failures {failures}")
 
 
-
-
 if __name__ == "__main__":
     main()
 
 
-print(f"MU_DICT: {MU_DICT}")
+"""print(f"MU_DICT: {MU_DICT}")
 print(f"SCENE_DICT: {SCENE_DICT}")
 print(f"TASK_INFO: {TASK_INFO}")
-print(get_affordance_regions(OBJECTS_DICT, True))
+print(get_affordance_regions(OBJECTS_DICT, True))"""
